a8484e274cb17a0a2c22e4930963c5f9efdad8c0
Alin Apostol <alin.cristian977@gmail.com>
2022-02-19 14:16:44 +0200

    1-level directories search

--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -1,9 +1,20 @@
-﻿namespace ConsoleUI
+﻿using System;
+using System.IO;
+
+namespace ConsoleUI
 {
     internal class Program
     {
         public static void Main(string[] args)
         {
+            string rootPath = @"E:\Temp\Demos\FileSystem";
+
+            string[] directories = Directory.GetDirectories(rootPath);
+
+            foreach (string directory in directories)
+            {
+                Console.WriteLine(directory);
+            }
         }
     }
 }
