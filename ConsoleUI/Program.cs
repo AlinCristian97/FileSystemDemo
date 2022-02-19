a6ec83aee0b8d4c47ec4b83406d0b790a54b6f39
Alin Apostol <alin.cristian977@gmail.com>
2022-02-19 16:53:38 +0200

    reading byte[] from file

--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -56,6 +56,19 @@ namespace ConsoleUI
             byte[] rsByteArray = Encoding.Default.GetBytes(randomString);
             
             fs.Write(rsByteArray, 0, rsByteArray.Length);
+            
+            fs.Position = 0;
+
+            byte[] fileByteArray = new byte[rsByteArray.Length];
+
+            for (int i = 0; i < rsByteArray.Length; i++)
+            {
+                fileByteArray[i] = (byte) fs.ReadByte();
+            }
+
+            Console.WriteLine(Encoding.Default.GetString(fileByteArray));
+            
+            fs.Close();
         }
     }
 }
