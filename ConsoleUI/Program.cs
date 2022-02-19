442c3b29e309946ab66c39b7d8b0ca636201bf9c
Alin Apostol <alin.cristian977@gmail.com>
2022-02-19 13:41:58 +0200

    updated gitignore

--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -1,9 +1,9 @@
 ﻿namespace ConsoleUI
 {
-  internal class Program
-  {
-    public static void Main(string[] args)
+    internal class Program
     {
+        public static void Main(string[] args)
+        {
+        }
     }
-  }
 }
